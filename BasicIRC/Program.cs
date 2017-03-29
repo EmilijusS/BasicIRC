0cba792014eefed28115ddb49b20ed786a3abfeb
Emilijus Stankus <emilijus@gmail.com>
2017-03-29 22:41:31 +0300

    Created ConnectForm interface.

--- BasicIRC/Program.cs
+++ BasicIRC/Program.cs
@@ -16,7 +16,9 @@ namespace BasicIRC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ConnectForm());
+
+            //new Test().Start();
         }
     }
 }
---
+++ BasicIRC/Test.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicIRC
+{
+    class Test
+    {
+        public void Start()
+        {
+            int port = 6667;
+            byte[] data;
+            TcpClient client = new TcpClient("chat.freenode.net", port);
+            NetworkStream stream = client.GetStream();
+            string input;
+
+            Thread listen = new Thread(() => GetResponse(stream));
+            listen.Start();
+
+            data = Encoding.ASCII.GetBytes("NICK squotro\r\n");
+
+            stream.Write(data, 0, data.Length);
+
+            data = Encoding.ASCII.GetBytes("USER squotro 0 * :Real name\r\n");
+
+            stream.Write(data, 0, data.Length);
+
+            while(true)
+            {
+                input = Console.ReadLine();
+
+                input += "\r\n";
+                data = Encoding.ASCII.GetBytes(input);
+                stream.Write(data, 0, data.Length);
+            }
+
+            listen.Join();
+          
+            stream.Close();
+            client.Close();
+        }
+
+        void GetResponse(NetworkStream stream)
+        {
+            int bytes;
+            var data = new Byte[1024];
+
+            while(true)
+            {
+                bytes = stream.Read(data, 0, data.Length);
+
+                if(bytes > 0)
+                {
+                    Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
